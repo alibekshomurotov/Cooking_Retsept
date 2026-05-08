81d9694004a2b18d05e37c31ec686ecbcaedbdfd
Shomurotov Alibek <umidbekshomurotov3@gmail.com>
2026-05-08 09:46:53 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Cooking_Retsept/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.themealdb.com/api/json/v1/1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
   }
 })
