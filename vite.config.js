04c287993e70af2350d179f1e14ea280fe6e5506
Shomurotov Alibek <umidbekshomurotov3@gmail.com>
2026-05-08 09:48:50 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Cooking_Retsept/',
-  }
+  base: '/Cooking_Retsept/', // repository nomingizga moslang
 })
